64d61216334a6ffb59670d7f8a2bc6e5b156b748
LyleRomero <lyle.planet@gmail.com>
2025-05-27 22:52:15 -0500

    Diseño hero

--- src/pages/home/sections/Hero.tsx
+++ src/pages/home/sections/Hero.tsx
@@ -14,12 +14,15 @@ const Hero = () => {
 
       {/* Contenido encima (opcional) */}
       <div className="absolute inset-0 flex items-center justify-center text-center px-4">
-        <div className="text-white max-w-2xl">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4 drop-shadow-xl libre-baskerville-regular leading-tight">
-            Cuidado consciente, belleza esencial
+        <div className="max-w-2xl mt-40">
+          <h1 className="md:text-6xl font-bold inter-title leading-none">
+            <span className="text-[#F3F1E7] text-4xl md:text-5xl block">
+              Cosmética que honra la tierra.</span>
+            <span className="text-[#E5B38D] text-5xl md:text-6xl block pl-30">
+              Y tu piel.</span>
           </h1>
-          <button className="mt-4 bg-[#E8C88A] text-[#1A1A1A] px-6 py-3 rounded-2xl hover:bg-[#4E5F46] hover:text-white transition-all duration-300 shadow-md inter-navegacion">
-            Ver Catálogo
+          <button className="m-8 bg-[#E8C88A] text-[#4E5F46] px-3 py-3 rounded-full hover:bg-[#A98263] hover:text-[#EDE8DF] transition-all duration-300 shadow-md inter-title">
+            Descubre nuestros productos
           </button>
         </div>
       </div>
